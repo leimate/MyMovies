8b2d3fe202c82ed5a63fe97fd171218c86a46a29
leimate <swift.mate431@gmail.com>
2024-09-21 21:00:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const movies = [
     {
         title: "Movie 1",
-        image: "link_to_image_1.jpg", // Replace with your actual image link
+        image: "https://filipinotimes.net/wp-content/uploads/2019/08/Screen-Shot-2019-08-05-at-1.35.13-PM-1.png", // Replace with your actual image link
         video: "https://drive.google.com/file/d/1SZSgeHm52VtmkvEB8CBGq5UFNiRGOot0/preview" // Google Drive preview link
     },
+    {
+        title: "Movie 2",
+        image: "https://asset-ent.abs-cbn.com/ent/entertainment/media/star-cinema/2023/03/0327/kathryn-bernardo-announces-2-powerful-movies-under-abs-cbn-films-pa-1.jpg", // Replace with your actual image link
+        video: "https://drive.google.com/file/d/1vZR2IPB0TQwT_8vNrvAFz0kS6h1uLdyv/preview" // Google Drive preview link
+    },
+    
     // Add more movie objects as needed
 ];
 

6227a688aa2e1e31fa62c712737d7fd054ebb59b
leimate <swift.mate431@gmail.com>
2024-09-21 20:18:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const movies = [
     {
         title: "Movie 1",
         image: "https://filipinotimes.net/wp-content/uploads/2019/08/Screen-Shot-2019-08-05-at-1.35.13-PM-1.png",
-        video: "https://drive.google.com/file/d/1SZSgeHm52VtmkvEB8CBGq5UFNiRGOot0/view?usp=sharing"
+        video: "https://drive.google.com/uc?export=preview&id=1SZSgeHm52VtmkvEB8CBGq5UFNiRGOot0"
     },
     {
         title: "Movie 2",
